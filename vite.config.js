24e6109616350c1e01be25f7bb8df71fc58111a6
bhavikatewari <bhavikatewari@gmail.com>
2026-07-18 16:20:21 +0530

    Update base path in vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/frontend-for-clone-/'  // <-- ye line sabse important hai
+    // <-- ye line sabse important hai
 })
